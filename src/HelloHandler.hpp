5025e1e571a51cecc1f6050d1c4efe8c6f7e25a5
Luke Nihlen <luke.nihlen@gmail.com>
2020-07-17 22:03:26 -0700

    wip on ssl and /hello

--- src/HelloHandler.hpp
+++ src/HelloHandler.hpp
@@ -1,14 +1,14 @@
-#ifndef SRC_DUMP_HANDLER_HPP_
-#define SRC_DUMP_HANDLER_HPP_
+#ifndef SRC_HELLO_HANDLER_HPP_
+#define SRC_HELLO_HANDLER_HPP_
 
 #include <proxygen/httpserver/RequestHandler.h>
 #include <proxygen/httpserver/RequestHandlerFactory.h>
 
 namespace garg { namespace handler {
 
-class DumpHandler : public proxygen::RequestHandler {
+class HelloHandler : public proxygen::RequestHandler {
 public:
-    explicit DumpHandler(const std::string& dumpPath);
+    HelloHandler();
 
     void onRequest(std::unique_ptr<proxygen::HTTPMessage> request) noexcept override;
     void onBody(std::unique_ptr<folly::IOBuf> body) noexcept override;
@@ -16,28 +16,17 @@ public:
     void onUpgrade(proxygen::UpgradeProtocol proto) noexcept override;
     void requestComplete() noexcept override;
     void onError(proxygen::ProxygenError err) noexcept override;
-
-private:
-    void writeFile(folly::EventBase* evb);
-    bool m_valid;
-    std::string m_dumpPath;
-
-    std::string m_guid;
-    int m_dumpFile;
-    std::unique_ptr<folly::IOBuf> m_body;
 };
 
-class DumpHandlerFactory : public proxygen::RequestHandlerFactory {
+class HelloHandlerFactory : public proxygen::RequestHandlerFactory {
 public:
-    explicit DumpHandlerFactory(const std::string& dumpPath);
+    explicit HelloHandlerFactory();
     void onServerStart(folly::EventBase*) noexcept override;
     void onServerStop() noexcept override;
     proxygen::RequestHandler* onRequest(proxygen::RequestHandler*, proxygen::HTTPMessage*) noexcept override;
-private:
-    std::string m_dumpPath;
 };
 
 } // namespace handler
 } // namespace garg
 
-#endif // SRC_DUMP_HANDLER_HPP_
+#endif // SRC_HELLO_HANDLER_HPP_
--- src/gargamelle.cpp
+++ src/gargamelle.cpp
@@ -1,12 +1,19 @@
 #include "DumpHandler.hpp"
+#include "HelloHandler.hpp"
 
 #include <folly/portability/GFlags.h>
 #include <folly/executors/CPUThreadPoolExecutor.h>
 #include <proxygen/httpserver/HTTPServer.h>
+#include <wangle/ssl/SSLContextConfig.h>
 
 DEFINE_int32(port, 8080, "Which port to bind for incoming HTTP(S) traffic.");
 DEFINE_string(dumpPath, "", "Root path for saving incoming crash dumps.");
 DEFINE_int32(numThreads, 2, "Number of threads to run on the server.");
+
+DEFINE_bool(useSSL, false, "Enable SSL on the bound connection.");
+DEFINE_string(certificatePath, "", "Path to the SSL certificate.");
+DEFINE_string(keyPath, "", "Path to the SSL key.");
+DEFINE_string(keyPassword, "", "SSL key password.");
 
 int main(int argc, char* argv[]) {
     gflags::ParseCommandLineFlags(&argc, &argv, true);
@@ -17,17 +24,25 @@ int main(int argc, char* argv[]) {
             FLAGS_numThreads, std::make_shared<folly::NamedThreadFactory>("StaticDiskIOThread"));
     folly::setCPUExecutor(diskIOThreadPool);
 
+
     std::vector<proxygen::HTTPServer::IPConfig> IPs = {
         { folly::SocketAddress("0.0.0.0", FLAGS_port, true), proxygen::HTTPServer::Protocol::HTTP }
     };
+
+    if (FLAGS_useSSL) {
+        wangle::SSLContextConfig sslConfig;
+        sslConfig.setCertificate(FLAGS_certificatePath, FLAGS_keyPath, FLAGS_keyPassword);
+        IPs[0].sslConfigs.push_back(sslConfig);
+    }
 
     proxygen::HTTPServerOptions options;
     options.threads = FLAGS_numThreads;
     options.idleTimeout = std::chrono::milliseconds(60000);
     options.shutdownOn = {SIGINT, SIGTERM};
     options.enableContentCompression = true;
-    options.handlerFactories = proxygen::RequestHandlerChain().addThen<garg::handler::DumpHandlerFactory>(
-            FLAGS_dumpPath).build();
+    options.handlerFactories = proxygen::RequestHandlerChain()
+        .addThen<garg::handler::DumpHandlerFactory>(FLAGS_dumpPath)
+        .addThen<garg::handler::HelloHandlerFactory>().build();
     options.h2cEnabled = false;
 
     proxygen::HTTPServer server(std::move(options));
